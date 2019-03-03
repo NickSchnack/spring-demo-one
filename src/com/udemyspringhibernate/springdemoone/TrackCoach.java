a53ac6d3c2b2ad5e1c483188d10b1dabd05faf4a
Nick <nicholas.schnack@cuchicago.edu>
2019-03-03 17:09:41 -0600

    Uploading recent changes

--- src/com/udemyspringhibernate/springdemoone/TrackCoach.java
+++ src/com/udemyspringhibernate/springdemoone/TrackCoach.java
@@ -2,6 +2,12 @@ package com.udemyspringhibernate.springdemoone;
 
 public class TrackCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public TrackCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k";
--- src/com/udemyspringhibernate/springdemoone/VolleyballCoach.java
+++ src/com/udemyspringhibernate/springdemoone/VolleyballCoach.java
@@ -2,9 +2,16 @@ package com.udemyspringhibernate.springdemoone;
 
 public class VolleyballCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public VolleyballCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Perform 15 spikes and 5 minutes of volleys";
     }
 
 }
+
